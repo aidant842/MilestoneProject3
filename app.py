f8cf0e1d7f4b65c2cec5c525f80d9a7f8408f26c
aidant842 <aidant842@gmail.com>
2020-05-21 10:33:44 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if os.path.exists("env.py"):
+   import env
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'gameDB'
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/home_page')
+def home_page():
+    return render_template('index.html', games=mongo.db.games.find())
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)

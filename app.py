031b18ba459404ea4f7541cd5f5ba38b8060d68c
aidant842 <aidant842@gmail.com>
2020-05-28 14:14:13 +0000

    changed game header to capatilize added update_game route with corresponding function to update info based on user input changed column sizing for login and signup pages fixed an issue where select tags from edit form would send a null value to database

--- app.py
+++ app.py
@@ -46,14 +46,39 @@ def addGame():
 @app.route('/insert_game', methods = ['POST'])  
 def insert_game():
     game = mongo.db.games
-    game.insert(request.form.to_dict(), {'complete': 'false'})
+    game.insert_one(request.form.to_dict())
     return redirect(url_for('games')) 
 
 @app.route('/edit_game/<game_id>')
 def edit_game(game_id):
     the_game = mongo.db.games.find_one({'_id': ObjectId(game_id)})
     return render_template('edit_game.html', game=the_game, genre=mongo.db.genre.find(), platform=mongo.db.platform.find(),
-                            vr=mongo.db.vr_capable.find(), age_rating=mongo.db.age_rating.find())                             
+                            vr=mongo.db.vr_capable.find(), age_rating=mongo.db.age_rating.find())
+
+@app.route('/update_game/<game_id>', methods=['POST'])
+def update_game(game_id):
+    game = mongo.db.games
+    game.update_one({'_id' : ObjectId(game_id)},
+    
+    {"$set": 
+    {
+        'title': request.form.get('title'),
+        'genre_name': request.form.get('genre_name'),
+        'description': request.form.get('description'),
+        'shop_link': request.form.get('shop_link'),
+        'review': request.form.get('review'),
+        'age_rating': request.form.get('age_rating'),
+        'image': request.form.get('image'),
+        'platform': request.form.get('platform'),
+        'release_date': request.form.get('release_date'),
+        'languages': request.form.get('languages'),
+        'developer': request.form.get('developer'),
+        'trailer_link': request.form.get('trailer_link'),
+        'playthrough_time': request.form.get('playthrough_time'),
+        'vr_capable': request.form.get('vr_capable')
+    }})
+
+    return redirect(url_for('games'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),

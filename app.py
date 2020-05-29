e665b994b8346014c456030527c741ae67303638
aidant842 <aidant842@gmail.com>
2020-05-29 17:15:37 +0000

    added link to signup page on login page added logic to signup route, now works and sends user data to db added login to login route, which stores username in new session on successful login

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, flash, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
 if os.path.exists("env.py"):
    import env
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'gameDB'
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 mongo = PyMongo(app)
 
@@ -19,7 +21,7 @@ mongo = PyMongo(app)
 def home_page():
     return render_template('index.html')
 
-@app.route('/games')
+@app.route('/games', methods=['POST', 'GET'])
 def games():
 
     return render_template('games.html', genre=mongo.db.genre.find(), games=mongo.db.games.find(),
@@ -30,12 +32,9 @@ def games():
 def search():
     
     games = mongo.db.games
-    query=games.find({
-        'genre_name': request.form.get('genre'),
-        'age_rating': request.form.get('age'),
-        'platform'  : request.form.get('platform'),
-        'developer' : request.form.get('developer')
-    }) 
+    query = games.find({
+        'genre_name': request.form.get('genre')
+    })
 
     return render_template('games.html', genre=mongo.db.genre.find(), games=query,
                             age_rating=mongo.db.age_rating.find(), dev=mongo.db.developer.find(), plat=mongo.db.platform.find(),
@@ -47,12 +46,35 @@ def game_page(game_id):
     the_game = mongo.db.games.find_one({'_id': ObjectId(game_id)})
     return render_template('game.html', genre=mongo.db.genre.find(), game=the_game, games=mongo.db.games.find())
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    if request.method == 'POST':
+        users=mongo.db.users
+        user_login = users.find_one({'name': request.form.get('username')})
+
+        if user_login:
+            if check_password_hash(user_login['password'], request.form.get('password')):
+                session['username'] = request.form.get('username')
+                return 'you are logged in'
+            return 'Login Failed, please check your credentials!'
     return render_template('login.html')
 
-@app.route('/signup')
+    
+
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
+
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name': request.form.get('username')})
+
+        if not existing_user:
+            hashpw = generate_password_hash(request.form.get('password'))
+            users.insert_one({'name': request.form.get('username'), 'password': hashpw})
+            session['username'] = request.form.get('username')
+            return redirect(url_for('home_page'))
+        flash('that username already exists')
+
     return render_template('signup.html')
 
 @app.route('/addGame')
